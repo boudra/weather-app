68780371819c0e2815120dc3d5a04ad824a647a9
Mohamed Boudra <boudra.moha@gmail.com>
2020-05-04 13:47:30 +0200

    feat: https

--- src/apiService.js
+++ src/apiService.js
@@ -1,5 +1,5 @@
 export function getWeatherByName(cityName) {
-  return fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=3f4b891079045d352173506efa88c064`)
+  return fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=3f4b891079045d352173506efa88c064`)
     .then(response => response.json())
     .catch(err => {
       console.log(err, 'summat went wrong - name not found')
@@ -7,7 +7,7 @@ export function getWeatherByName(cityName) {
   };
 
 export function getWeatherByCoordinates(lat, long) {
-  return fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=3f4b891079045d352173506efa88c064`)
+  return fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=3f4b891079045d352173506efa88c064`)
     .then(response => response.json())
     .catch(err => console.log(err, 'summat went wrong - coordinates not found'))
 };
