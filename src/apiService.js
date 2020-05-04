48fc85e1ffba71cbfcfa42b7023e2cfea10da580
Mohamed Boudra <boudra.moha@gmail.com>
2020-05-04 13:56:04 +0200

    feat: use app id from env

--- src/apiService.js
+++ src/apiService.js
@@ -1,5 +1,7 @@
+const APP_ID = process.env.REACT_APP_WEATHER_MAP_APP_ID;
+
 export function getWeatherByName(cityName) {
-  return fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=3f4b891079045d352173506efa88c064`)
+  return fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${APP_ID}`)
     .then(response => response.json())
     .catch(err => {
       console.log(err, 'summat went wrong - name not found')
@@ -7,7 +9,7 @@ export function getWeatherByName(cityName) {
   };
 
 export function getWeatherByCoordinates(lat, long) {
-  return fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=3f4b891079045d352173506efa88c064`)
+  return fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${APP_ID}`)
     .then(response => response.json())
     .catch(err => console.log(err, 'summat went wrong - coordinates not found'))
 };
